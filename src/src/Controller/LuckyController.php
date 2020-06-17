e73b53214bb52761cfbccc58ca7644d6e07dd2b7
Will1400 <will1400@edu.campusvejle.dk>
2020-06-17 09:42:15 +0200

    Added basic header

--- src/src/Controller/LuckyController.php
+++ src/src/Controller/LuckyController.php
@@ -1,12 +1,11 @@
 <?php
-namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
-
     /**
      * @Route("/lucky/number")
      */
